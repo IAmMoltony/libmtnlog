3b595dff061ff9ca270016d4bd4fc51e25cbba0e
IAmMoltony <koronavirusnyj@gmail.com>
2023-10-25 13:21:18 +0600

    Add library files

---
+++ include/mtnlog.h
@@ -0,0 +1,50 @@
+/**
+ * @file mtnlog.h
+ * @brief Moltony's logging library
+ * @author moltony
+*/
+
+#ifndef MTNLOG_H_
+#define MTNLOG_H_ 1
+
+/**
+ * @brief Enum representing logging level
+*/
+typedef enum
+{
+    LOG_INFO = 0, /**< Information: Status or progress of the program */
+    LOG_WARNING = 1, /**< Warning: Something is a bit off, but we can still continue */
+    LOG_ERROR = 2, /**< Error: We are in trouble! */
+} MtnLogLevel;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Initialize logging
+ * @param level logging level
+ * @param logFileName name of the log file
+*/
+void mtnlogInit(const MtnLogLevel level, const char *logFileName);
+
+/**
+ * @brief Set logging level
+ * @param level level
+*/
+void mtnlogSetLevel(const MtnLogLevel level);
+
+/**
+ * @brief Print a message to the log
+ * @param level level of the message
+ * @param format formatting string
+ * 
+ * Used the same way as `printf`.
+*/
+void mtnlogMessage(const MtnLogLevel level, const char *format, ...);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ source/mtnlog.c
@@ -0,0 +1,90 @@
+#include "mtnlog.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
+
+static MtnLogLevel _logLevel = LOG_INFO;
+static char *_logFileName = NULL;
+
+static const char *_logLevelNames[] = {
+    "INFO",
+    "WARNING",
+    "ERROR",
+};
+
+static char *_getTimeString(void)
+{
+    // idk what this does
+
+    char *rnTime = malloc(28 * sizeof(char));
+    struct tm *tminfo;
+    time_t tmr = time(NULL);
+    tminfo = localtime(&tmr);
+    strftime(rnTime, 28, "%Y-%m-%d %H:%M:%S", tminfo);
+    return rnTime;
+}
+
+void mtnlogInit(const MtnLogLevel level, const char *logFileName)
+{
+    mtnlogSetLevel(level); // set log level
+    _logFileName = (char *)logFileName; // set log file name
+
+    // open the log file
+    FILE *f = fopen(_logFileName, "w");
+    if (!f)
+    {
+        perror(_logFileName);
+        return;
+    }
+
+    // print that log started
+    char *rnTime = _getTimeString();
+    fprintf(f, " *** Log Started %s\n", rnTime);
+    free(rnTime);
+
+    fclose(f);
+}
+
+void mtnlogSetLevel(const MtnLogLevel level)
+{
+    _logLevel = level;
+}
+
+void mtnlogMessage(const MtnLogLevel level, const char *format, ...)
+{
+    va_list l;
+    va_list l2;
+    va_start(l, format);
+    va_copy(l2, l);
+
+    if (level >= _logLevel)
+    {
+        // print to stdout if the supplied level is greater than or equal to the current log level
+        printf("[%s] ", _logLevelNames[level]);
+        vprintf(format, l);
+        putchar('\n');
+    }
+
+    FILE *f = fopen(_logFileName, "a"); // open the log file in append mode
+    if (!f)
+    {
+        // print error and close the va lists
+        perror(_logFileName);
+        va_end(l);
+        va_end(l2);
+        return;
+    }
+
+	char *rnTime = _getTimeString(); // get current time and date as a string
+    fprintf(f, "[%s] %s ", _logLevelNames[level], rnTime); // print log level and current time and date into log
+    free(rnTime); // free the time and date string
+    vfprintf(f, format, l2); // print the message into file
+    fputc('\n', f); // add a newline
+
+    fclose(f); // close the log file
+
+    // close va lists
+    va_end(l);
+    va_end(l2);
+}
