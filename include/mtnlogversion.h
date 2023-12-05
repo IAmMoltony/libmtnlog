68b237c757489597473462b3b21b07ba11ef5993
Moltony <105192532+IAmMoltony@users.noreply.github.com>
2023-12-05 22:12:26 +0600

    Add version info file

---
+++ include/mtnlogversion.h
@@ -0,0 +1,14 @@
+/**
+ * @file mtnlogversion.h
+ * @brief Moltony's logging library version information
+ * @author moltony
+*/
+
+#ifndef MTNLOGVERSION_H_
+#define MTNLOGVERSION_H_ 1
+
+#define MTNLOG_MAJOR 1
+#define MTNLOG_MINOR 0
+#define MTNLOG_PATCH 0
+
+#endif
