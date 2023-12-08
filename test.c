56a3d183098d21d81df1e9cfb6ae1ce13b4d6fd7
Moltony <105192532+IAmMoltony@users.noreply.github.com>
2023-12-08 13:30:26 +0600

    Add logger test program

---
+++ test.c
@@ -0,0 +1,15 @@
+#include "mtnlog.h"
+#include "mtnlogversion.h"
+
+int main(void)
+{
+    mtnlogInit(LOG_WARNING, "test.log");
+    mtnlogMessage(LOG_INFO, "Hi world, I'm using MtnLog version %d.%d.%d!", MTNLOG_MAJOR, MTNLOG_MINOR, MTNLOG_PATCH);
+    mtnlogMessage(LOG_INFO, "Information");
+    mtnlogMessage(LOG_WARNING, "Warning!");
+    mtnlogMessage(LOG_ERROR, "ERROR!!!");
+    mtnlogSetLevel(LOG_INFO);
+    mtnlogMessage(LOG_INFO, "Color test: info");
+    mtnlogMessage(LOG_ERROR, "Color test: error");
+    mtnlogMessage(LOG_WARNING, "Color test: warning");
+}
