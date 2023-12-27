db1dc04600827ba7d5166b50a35d722803178602
IAmMoltony <koronavirusnyj@gmail.com>
2023-12-27 22:56:58 +0600

    Add docs do mtnlogversion.h

--- include/mtnlogversion.h
+++ include/mtnlogversion.h
@@ -7,8 +7,19 @@
 #ifndef MTNLOGVERSION_H_
 #define MTNLOGVERSION_H_ 1
 
+/**
+ * @brief Major MtnLog version
+ */
 #define MTNLOG_MAJOR 1
+
+/**
+ * @brief Minor MtnLog version
+ */
 #define MTNLOG_MINOR 4
+
+/**
+ * @brief MtnLog patch
+ */
 #define MTNLOG_PATCH 0
 
 #endif
